729aae6685396ff2d6d38f967d83d5f542f810ed
AdriCQ <adrian.capote95@zoho.com>
2022-03-15 12:07:06 -0400

    Add mdi-icons and boot files

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,30 +1,60 @@
 import { boot } from 'quasar/wrappers';
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestHeaders } from 'axios';
+import NairdaApi from 'src/api';
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $axios: AxiosInstance;
   }
 }
+export const baseURL = process.env.DEV
+  ? 'http://localhost:8000'
+  : 'https://srv.net';
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
-
-export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
-  app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
-  app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
+const $api = axios.create({
+  baseURL: `${baseURL}`,
+  timeout: 30000,
+  timeoutErrorMessage: 'Error en la red',
+  withCredentials: true,
 });
 
-export { api };
+export default boot(({ app }) => {
+  /**
+   * Api request Interceptor
+   */
+  $api.interceptors.request.use((_request) => {
+    /* Append content type header if its not present */
+    if (!(_request.headers as AxiosRequestHeaders)['Content-Type']) {
+      (_request.headers as AxiosRequestHeaders)['Content-Type'] =
+        'application/json';
+    }
+
+    /* Check if authorization is set */
+    if (!(_request.headers as AxiosRequestHeaders)['Authorization']) {
+      /* Check if the user is authenticated to send Bearer token */
+      const token = '2|ebxFLK0ppsvxwc4IyvmDoe1vTPKmZLfDC0nnhBga';
+      if (token && token.length > 0) {
+        (_request.headers as AxiosRequestHeaders).Authorization =
+          'Bearer ' + token;
+      } else {
+        /* Send the application authentication as Bearer token */
+        (_request.headers as AxiosRequestHeaders).Authorization =
+          'Bearer ApiToken';
+      }
+    }
+    /* Check if app header is set */
+    if (!(_request.headers as AxiosRequestHeaders)['Nairda-Token']) {
+      (_request.headers as AxiosRequestHeaders)['Nairda-Token'] =
+        '1|$2y$10$G1.nEO.oRi5nrVSpZFAhguSARzeNgcLoDvg.DgnWZKYMYphooKlUW';
+    }
+    return _request;
+  });
+
+  app.config.globalProperties.$axios = axios;
+
+  app.config.globalProperties.$api = $api;
+});
+
+const $nairdaApi = NairdaApi($api);
+
+export { $api, $nairdaApi };
