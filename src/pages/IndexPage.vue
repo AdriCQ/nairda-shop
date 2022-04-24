27156a598834b4e23b4cf0cb3d38dfe4cbeb9d2f
AdriCQ <adrian.capote95@zoho.com>
2022-04-24 17:37:36 -0400

    Cache home page data

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,9 +5,13 @@
         <div class="col-sm-6 col-md-4 col-xs-12" v-if="!isAuth()">
           <auth-widget />
         </div>
-        <div class="col-sm-6 col-md-4 col-xs-12">
-          <home-slider />
+        <!-- Announcements -->
+        <div class="col-sm-6 col-md-4 col-xs-12" v-if="announcements.length">
+          <announcemenst-slider :data="announcements" />
         </div>
+        <!-- / Announcements -->
+
+        <!-- categories -->
         <div
           class="col-sm-6 col-md-4 col-xs-12 q-gutter-y-sm"
           v-if="hasCategories"
@@ -15,25 +19,34 @@
           {{ hasCategories }}
           <category-slider />
         </div>
+        <!-- / categories -->
         <!-- Top Offers -->
-        <div class="col-sm-6 col-md-4 col-xs-12 q-gutter-y-sm">
-          <template v-if="stores.length">
-            <title-widget :data="{ title: 'Ofertas Destacadas' }" />
-            <div>
-              <offer-group :data="offers" />
-            </div>
-          </template>
+        <div
+          class="col-sm-6 col-md-4 col-xs-12 q-gutter-y-sm"
+          v-if="offers.length"
+        >
+          <title-widget :data="{ title: 'Ofertas Destacadas' }" />
+          <div>
+            <offer-group :data="offers.slice(0, 4)" />
+          </div>
+          <div v-if="offers.slice(4, offers.length).length">
+            <offers-slider :data="offers.slice(4, offers.length)" />
+          </div>
         </div>
         <!-- / Top Offers -->
 
         <!-- Top Stores -->
-        <div class="col-sm-6 col-md-4 col-xs-12 q-gutter-y-sm">
-          <template v-if="stores.length">
-            <title-widget :data="{ title: 'Afiliados' }" />
-            <div>
-              <store-group :data="stores" />
-            </div>
-          </template>
+        <div
+          class="col-sm-6 col-md-4 col-xs-12 q-gutter-y-sm"
+          v-if="stores.length"
+        >
+          <title-widget :data="{ title: 'Afiliados' }" />
+          <div>
+            <store-group :data="stores.slice(0, 4)" />
+          </div>
+          <div v-if="stores.slice(4, stores.length).length">
+            <stores-slider :data="stores.slice(4, stores.length)" />
+          </div>
         </div>
         <!-- / Top Stores -->
       </div>
@@ -44,16 +57,18 @@
 <script lang="ts" setup>
 import AuthWidget from 'src/components/widgets/AuthWidget.vue';
 import CategorySlider from 'src/components/sliders/CategorySlider.vue';
-import HomeSlider from 'src/components/sliders/HomeSlider.vue';
+// import HomeSlider from 'src/components/sliders/HomeSlider.vue';
+import AnnouncemenstSlider from 'src/components/sliders/AnnouncemenstSlider.vue';
 import StoreGroup from 'src/components/groups/StoresGroup.vue';
 import OfferGroup from 'src/components/groups/OffersGroup.vue';
 import TitleWidget from 'src/components/widgets/TitleWidget.vue';
-import { isAuth, notificationHelper } from 'src/helpers';
-import { computed, ref } from 'vue';
-import { IShopOffer, IShopStore } from 'src/api';
-import { $nairdaApi } from 'src/boot/axios';
-import { injectStrict, _shopCategory } from 'src/injectables';
+import OffersSlider from 'src/components/sliders/OffersSlider.vue';
+import StoresSlider from 'src/components/sliders/StoresSlider.vue';
+import { isAuth } from 'src/helpers';
+import { computed } from 'vue';
+import { injectStrict, _app, _shopCategory } from 'src/injectables';
 
+const $app = injectStrict(_app);
 const $categories = injectStrict(_shopCategory);
 /**
  * -----------------------------------------
@@ -61,31 +76,15 @@ const $categories = injectStrict(_shopCategory);
  * -----------------------------------------
  */
 const hasCategories = computed(() => $categories.available.length > 3);
-const offers = ref<IShopOffer[]>([]);
-const stores = ref<IShopStore[]>([]);
+const announcements = computed(() => $app.homeAnn);
+const offers = computed(() => $app.homeOffers);
+const stores = computed(() => $app.homeStores);
 /**
- * loadOffers
+ * -----------------------------------------
+ *	Init
+ * -----------------------------------------
  */
-async function loadOffers() {
-  try {
-    const resp = await $nairdaApi.ShopOffer.filter({ limit: 8 });
-    offers.value = resp.data;
-  } catch (error) {
-    notificationHelper.axiosError(error, 'No se pudo obtener las ofertas');
-  }
-}
-/**
- * loadStores
- */
-async function loadStores() {
-  try {
-    const resp = await $nairdaApi.ShopStore.filter({ limit: 8 });
-    stores.value = resp.data;
-  } catch (error) {
-    notificationHelper.axiosError(error, 'No se pudo obtener los Datos');
-  }
-}
-
-loadOffers();
-loadStores();
+$app.loadAnnouncements();
+$app.loadOffers();
+$app.loadStores();
 </script>
